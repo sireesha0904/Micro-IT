971962184fa978aa40eef135a30b048a092a87f2
Modi Sireesha <modisireesha09@gmail.com>
2025-05-26 22:39:17 +0530

    Quiz Game

--- Quize Game/client/quiz-game-frontend/src/App.jsx
+++ Quize Game/client/quiz-game-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import StartQuiz from "./pages/StartQuiz"; // Import the Quiz component
-import QuizList from "./pages/QuizList";  
+import QuizList from "./pages/QuizList";
 import "./App.css";
 // Import Quiz component when ready
 
@@ -13,7 +13,6 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/quizzes" element={<QuizList />} />
       <Route path="/quiz/:quizId" element={<StartQuiz />} />
-
       <Route path="/auth" element={<Auth />} />
 
       {/* <Route path="/quiz/:quizId" element={<Quiz />} /> */}
